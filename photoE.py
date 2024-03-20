b65a1bd0711bb81c2286a164ba60071bccee5d10
Daniel Deutsch <deu42236@spseol.cz>
2024-03-20 13:54:44 +0100

    added option to select you own .img or .jpg file

--- photoE.py
+++ photoE.py
@@ -48,6 +48,16 @@ def filter3():
 pic = "trouba.jpg"
 obrazek = Image.open(pic)
 sirka, vyska = obrazek.size
+print("Would you like to choose your own picture?")
+if  input("yes or no: ") == "yes":
+    pic = input("Enter the name of the picture: ")
+    obrazek = Image.open(pic)
+    sirka, vyska = obrazek.size
+else:
+    obrazek = Image.open(pic)
+    sirka, vyska = obrazek.size
+    print("Ok, we chose the picture for you. It is called 'trouba.jpg' and it is in the same folder as this program.")
+
 inp = input("Choose filter 1, 2 or 3: ")
 start_time = time.time()
 if inp == "1":
