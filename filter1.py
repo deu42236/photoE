afb74be7c6f55f3703164e019f8c9376a5a51b64
Daniel Deutsch <deu42236@spseol.cz>
2024-03-20 13:24:53 +0100

    improved runtime by changing while -> for loop

--- filter1.py
+++ filter1.py
@@ -5,9 +5,9 @@ start_time = time.time()
 obrazek = Image.open("trouba.jpg")
 sirka, vyska = obrazek.size
 x = 0
-while x < sirka:
+for x in range(sirka):
     y = 0
-    while y < vyska:
+    for y in range(vyska):
         r, g, b = obrazek.getpixel((x,y))
         prumer = int((r+g+b)/3)
         obrazek.putpixel((x,y), (r , b, r))
@@ -20,8 +20,6 @@ while x < sirka:
 print("ok")
 ##display(obrazek)
 end_time = time.time()
-
-print(end_time - start_time)
-obrazek.show()
 print(f"Runtime of the program is {end_time - start_time}")
-print(end_time - start_time)
+
+obrazek.show()
